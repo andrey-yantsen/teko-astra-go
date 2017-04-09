e8bdeb09a84c7268859779376edfc353abea3da4
Andrey Yantsen <andrey-yantsen@users.noreply.github.com>
2017-04-09 13:48:20 +0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,28 @@
+GOOS := $(shell go env GOOS)
+GOARCH := $(shell go env GOARCH)
+GOENV_EXTRA :=
+
+DEB_TARGET_ARCH ?=
+
+ifeq ($(DEB_TARGET_ARCH),armel)
+GOARCH := arm
+GOOS := linux
+GOENV_EXTRA := GOARM=5 CC_FOR_TARGET=arm-linux-gnueabi-gcc CC=$$CC_FOR_TARGET CGO_ENABLED=1
+endif
+ifeq ($(DEB_TARGET_ARCH),armhf)
+GOARCH := arm
+GOOS := linux
+GOENV_EXTRA := GOARM=6 CC_FOR_TARGET=arm-linux-gnueabihf-gcc CC=$$CC_FOR_TARGET CGO_ENABLED=1
+endif
+
+CURDIR := $(shell pwd)
+GOENV := GOOS=$(GOOS) GOARCH=$(GOARCH) $(GOENV_EXTRA)
+
+deps:
+	cd $$(go env GOPATH)\
+	  && wget -O $(GOOS)_$(GOARCH).tar.gz $$(curl -s https://api.github.com/repos/andrey-yantsen/teko-astra-go/releases/latest | fgrep browser_download_url | cut -d'"' -f 4 | fgrep _$(GOOS)_$(GOARCH).)\
+	  && tar -zxf $(GOOS)_$(GOARCH).tar.gz\
+	  && rm $(GOOS)_$(GOARCH).tar.gz
+
+build:
+	$(GOENV) go build ./cmd/astra
---
+++ cmd/astra/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+
+	"fmt"
+
+	astra_l "github.com/andrey-yantsen/teko-astra-go"
+)
+
+func main() {
+	serial := flag.String("serial", "/dev/ttyAPP1", "serial port device (/dev/...)")
+	address := flag.Uint("address", 255, "device address")
+	action_find := flag.Bool("find-device", false, "run FindDevice command")
+	action_register := flag.Bool("register-device", false, "run RegisterDevice command")
+	action_delete := flag.Bool("delete-device", false, "run DeleteDevice command")
+	action_createl2net := flag.Uint("create-l2-net", 99999999, "(re-)create Level2 net")
+	action_registerl2dev := flag.Bool("register-l2-device", false, "register new Level2 device")
+	action_getstate := flag.Bool("get-state", false, "get RI-M status")
+	action_getevents := flag.Bool("get-events", false, "get RI-M events")
+	flag.Parse()
+	if driver, err := astra_l.Connect(*serial); err != nil {
+		panic(err)
+	} else {
+		device := driver.GetDevice(uint16(*address))
+		if *action_find {
+			if f, err := device.FindDevice(); err != nil {
+				panic(err)
+			} else {
+				fmt.Printf("FindDevice response: %+v\n", f)
+			}
+		}
+		if *action_delete {
+			if err := device.DeleteDevice(); err != nil {
+				panic(err)
+			} else {
+				fmt.Printf("Device %+v deleted\n", *address)
+			}
+		}
+		if *action_register {
+			broadcastDevice := driver.GetDevice(255)
+			if f, err := broadcastDevice.FindDevice(); err != nil {
+				panic(err)
+			} else if err := device.RegisterDevice(f.EUI.GetShortDeviceEUI()); err != nil {
+				panic(err)
+			} else {
+				fmt.Printf("Device %+v registered\n", *address)
+			}
+		}
+		if *action_getstate {
+			if s, err := device.GetState(); err != nil {
+				panic(err)
+			} else {
+				fmt.Printf("Device state: %+v\n", s)
+			}
+		}
+		if *action_getevents {
+			if e, err := device.GetEvents(); err != nil {
+				panic(err)
+			} else {
+				fmt.Printf("Device events: %+v\n", e)
+			}
+		}
+		if *action_createl2net < 4 {
+			if err := device.CreateLevel2Net(uint8(*action_createl2net)); err != nil {
+				panic(err)
+			}
+		}
+		if *action_registerl2dev {
+			if dev, err := device.RegisterLevel2Device(0); err != nil {
+				panic(err)
+			} else {
+				fmt.Printf("Registered new L2 device: %+v\n", dev)
+			}
+		}
+	}
+}
